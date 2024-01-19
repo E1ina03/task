7a1758850484afb143fb7114edcb9cdd9403b314
E1ina03 <elinasargsyan0323@gmail.com>
2024-01-19 15:51:45 +0400

    adding functionality for sorting

--- app/app/Services/AdminServices.php
+++ app/app/Services/AdminServices.php
@@ -18,43 +18,45 @@ class AdminServices
     {
         $user = Auth::user();
         $filter = $request->get('enable');
-
+        $sorting = $request->get('name');
         try {
             $filteredUsers = [];
-            if ($user->role_id == 2)
-            {
-                $users = User::query()->get();
 
-                if ($filter == null)
-                {
+            if ($user->role_id == 2) {
+                if ($sorting == 'asc'|| $sorting == 'desc'){
+                    $users = User::query()->orderBy('name', $sorting)->get();
+            }
+                else{
+                    $users = User::query()->get();
+                }
+
+                if ($filter == null) {
                     $usersData = $users->map(function (User $user) {
-                       return  [
-                                'id' => $user->id,
-                                'name' => $user->name,
-                                'email' => $user->email,
-                                'role' => $user->role->name,
-                                'product' => $user->products,
+                        return [
+                            'id' => $user->id,
+                            'name' => $user->name,
+                            'email' => $user->email,
+                            'role' => $user->role->name,
+                            'product' => $user->products,
                         ];
                     })->toArray();
 
-                    return   ['users' => $usersData];
+                    return ['users' => $usersData];
                 }
-                foreach ($users as $currentUser)
-                {
-                    if (($filter == EnumForEnable::ENABLED->value || $filter == EnumForEnable::DISABLED->value )
-                        && $currentUser->enable == $filter)
-                    {
-                        $filteredUsers[] = [
-                            'id' => $currentUser->id,
-                            'name' => $currentUser->name,
-                            'email' => $currentUser->email,
-                            'role' => $currentUser->role->name,
-                            'product' => $currentUser->products,
-                        ];
+                foreach ($users as $currentUser) {
+                    if (($filter == EnumForEnable::ENABLED->value || $filter == EnumForEnable::DISABLED->value)
+                        && $currentUser->enable == $filter) {
+                        $filteredUsers[] =
+                            [
+                                'id' => $currentUser->id,
+                                'name' => $currentUser->name,
+                                'email' => $currentUser->email,
+                                'role' => $currentUser->role->name,
+                                'product' => $currentUser->products,
+                            ];
                     }
                 }
-            }
-            return ['users' => $filteredUsers] ;
+            }return ['users' => $filteredUsers];
         }
         catch (\Exception)
         {
