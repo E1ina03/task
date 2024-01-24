7c4c25b33df85346c8d76196c651b403ee3966ee
E1ina03 <elinasargsyan0323@gmail.com>
2024-01-24 16:40:31 +0400

    fixed code review remark

--- app/app/Services/AdminServices.php
+++ app/app/Services/AdminServices.php
@@ -34,17 +34,21 @@ class AdminServices
                     $users = User::query()->orderBy('name', $filter['name'])
                         ->paginate($perPage, page: $page);
                 }
-                if (isset($filter['firstname'])){
-                    if (isset($filter['name'])){
+                if (isset($filter['firstname']))
+                {
+                    if (isset($filter['name']))
+                    {
                         $users = User::query()->where('name',$filter['firstname'])
                             ->orderBy('name', $filter['name'])
                             ->paginate($perPage, page: $page);
                     }
-                   else{
+                   else
+                   {
                        $users = User::query()->where('name',$filter['firstname'])
                            ->paginate($perPage, page: $page);
                    }
                 }
+
                 if (isset($filter['enable']) === false)
                 {
                     $usersTotal = $users->total();
@@ -53,32 +57,38 @@ class AdminServices
                         'total' => $usersTotal
                     ];
                 }
+
                 if (isset($filter['enable']))
                 {
                     if (isset($filter['name']))
                     {
                         $filteredUsers = User::query()->where('enable', $filter['enable'])
-                            ->orderBy('name', $filter['name'])->paginate($perPage, page: $page);
+                            ->orderBy('name', $filter['name'])
+                            ->paginate($perPage, page: $page);
                     }
-                    elseif (isset($filter['firstname'])){
-                        $filteredUsers = User::query()->where('enable', $filter['enable'])
-                            ->where('name', $filter['firstname'])->paginate($perPage, page: $page);
-                    }
-                    elseif (isset($filter['name']) && isset($filter['firstname'])){
+                    elseif (isset($filter['firstname']))
+                    {
                         $filteredUsers = User::query()->where('enable', $filter['enable'])
                             ->where('name', $filter['firstname'])
-                            ->orderBy('name', $filter['name'])->paginate($perPage, page: $page);
+                            ->paginate($perPage, page: $page);
+                    }
+                    elseif (isset($filter['name']) && isset($filter['firstname']))
+                    {
+                        $filteredUsers = User::query()->where('enable', $filter['enable'])
+                            ->where('name', $filter['firstname'])
+                            ->orderBy('name', $filter['name'])
+                            ->paginate($perPage, page: $page);
                     }
                     else
                     {
-                        $filteredUsers = User::query()->where('enable', $filter['enable'])->paginate(
+                        $filteredUsers = User::query()->where('enable', $filter['enable'])
+                            ->paginate(
                             $perPage,
                             page: $page
                         );
                     }
                 }
             }
-
             $filteredUsersTotal = $filteredUsers->total();
             return ['users' => $filteredUsers, 'total' => $filteredUsersTotal];
         }
